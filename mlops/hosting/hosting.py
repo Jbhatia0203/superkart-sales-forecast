b1b653be786f36625b0666d19cab3165db5b9bb6
Jbhatia0203 <86400361+Jbhatia0203@users.noreply.github.com>
2026-04-21 10:16:27 +0530

    Update hosting.py. correcting folder path for mlops

--- mlops/hosting/hosting.py
+++ mlops/hosting/hosting.py
@@ -7,7 +7,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 
 # upload files in deployment folder - dockerfile,app.py, dependencies to hf space
 api.upload_folder(
-    folder_path="/content/mlops/deployment",
+    folder_path="mlops/deployment",
     repo_id="JaiBhatia020373/mlops",
     repo_type="space",
     path_in_repo=""
